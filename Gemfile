12a017f33a947bbabe82ec235c480d1831c47aa0
ianstalter123 <istalter66@gmail.com>
2016-09-29 20:23:33 -0700

    added seed rb with customers and contracts

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 
+gem 'capybara'
+
 gem 'bootstrap-sass'
 
 gem 'rails-pry'
@@ -45,6 +47,7 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Contract.destroy_all
+Customer.destroy_all
+
+ary = Array.new
+
+50.times do |index|
+	c = Customer.create!(name: Faker::Name.name,
+		address: Faker::Address.city)
+	ary.push(c)
+end
+
+ary.each { |a|
+	40.times do
+		Contract.create!(car_model: Faker::Company.name,
+			driver_name: Faker::Name.name, due_date: Faker::Date.forward(23),
+			customer_id: a.id)
+	end
+}
+
+p "Created #{Customer.count} customers"
